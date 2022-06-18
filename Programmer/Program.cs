a7049984854c2b3d6be03d9bee22b510f81bb058
Orkhansha <orkhansha@code.edu.az>
2022-06-19 02:51:03 +0400

    Added program files

---
+++ Programmer/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Programmer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verilmish n ededinin 3-e ve 7-ye bolunub bolunmemesini tapmaq.
+
+            //int n = 0;
+            //int m = 100;
+            //for (int i = n; i < m; i++)
+            //{
+            //    if (i % 21 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+
+
+
+            //Verilmish n ve m ededlerinin her ikisi cutdurse n ve m ededlerinin cemini tapmaq tapmaq.
+
+            //int n = 6;
+            //int m = 8;
+            //int sum = 0;
+
+            //if (n % 2 == 0 && m % 2 == 0)
+            //{
+            //    Console.WriteLine(sum = n + m);
+            //}
+
+
+
+
+
+
+
+
+
+            //verilmis n ve m ededleri arasindaki tek ededlerin cemini tapmaq   
+
+
+            //int n = 1;
+            //int m = 7;
+            //int sum = 0;
+
+            //for (int i = n; i < m; i++) 
+            //{
+            //    if (i % 2 == 1) 
+            //    {
+            //        sum = sum+i;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+            //Verilmis arrayin icindeki tek ededlerin cemini tapin.
+
+
+            //int [] numbers = { 1,5,8,18,9,52,54};
+            //int sum = 0;
+
+            //for (int i = 0; i < numbers.Length; i++) 
+            //{
+            //    if (numbers[i] % 2 == 1) 
+            //    {
+            //        sum += numbers[i];
+            //    }
+            //}
+
+            //Console.WriteLine(sum);
+
+
+
+
+
+
+
+            //Verilmis arrayin icindeki cut ededlerin sayini tapin.
+
+
+            //int[] numbers = { 1, 5, 8, 18, 9, 52, 76,54 };
+            //int count = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+        }
+    }
+}
